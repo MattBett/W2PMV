8163fed428d6e48a7c67dbcf1d06b1a256790a3d
MattBett <bettinger.matthieu@gmail.com>
2019-07-20 22:52:42 +0200

    Sketch

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const app = express();
-app.listen(3000, () => console.log('Listening at port 3000'));
+const fetch = require('node-fetch');
 app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+
+app.listen(3000, () => console.log('Listening at port 3000'));
+
+app.get('/api', function(req, res) {
+    res.send(getf());
+});
+
+async function getf() {
+    const response = await fetch('https://download.data.grandlyon.com/sos/velov?service=SOS&request=GetCapabilities');
+    return response;
+}
