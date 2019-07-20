23f2a353f29b8d830fddbe04e593ab2942e49b55
MattBett <bettinger.matthieu@gmail.com>
2019-07-21 00:05:30 +0200

    Added connextion to the server

---
+++ public/app.js
@@ -0,0 +1,43 @@
+const dev = true;
+
+const server = {
+    url: 'prout',
+    ip: '',
+    localhost: '127.0.0.1',
+    entryPoint: '3000',
+    service: '/api',
+
+    headers: new Headers(),
+    getInit: {
+        method: 'GET',
+        headers: this.headers,
+        mode: 'cors',
+        cache: 'default'
+    },
+
+    init: function() {
+        if(dev) {
+            server.url = 'http://' + server.localhost + ':' + server.entryPoint;
+        } else {
+            server.url = 'http://' + server.ip + ':' + server.entryPoint;
+        }     
+        
+        this.reqAPI = new Request(this.url + this.service, this.getInit);
+    }
+}
+
+const app = {
+    init: function() {
+        server.init();
+        this.refresh();
+        setInterval(this.refresh, 7500);
+    },
+
+    refresh: async function() {
+        var rawData = await fetch(server.reqAPI);
+        var parsedData = await rawData.text();
+        await console.log(parsedData);
+    }
+}
+
+app.init();
--- server.js
+++ server.js
@@ -7,10 +7,13 @@ app.use(express.json({ limit: '1mb' }));
 app.listen(3000, () => console.log('Listening at port 3000'));
 
 app.get('/api', function(req, res) {
-    res.send(getf());
+    res.send('Success !');
+    console.log('New connexion at /api');
 });
 
+/*
 async function getf() {
     const response = await fetch('https://download.data.grandlyon.com/sos/velov?service=SOS&request=GetCapabilities');
     return response;
 }
+*/
